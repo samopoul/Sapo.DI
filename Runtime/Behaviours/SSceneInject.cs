bd4bfd292ff5719de182db373bfa794449f9f6c4
Samuel Poul <samopoul@gmail.com>
2024-09-27 13:57:44 +0200

    feat: added feature to inject script from script

--- Runtime/Behaviours/SSceneInject.cs
+++ Runtime/Behaviours/SSceneInject.cs
@@ -13,11 +13,41 @@ namespace Sapo.DI.Runtime.Behaviours
     [DisallowMultipleComponent]
     public sealed class SSceneInject : MonoBehaviour, ISInjectorRegisterHandler
     {
+        private enum InjectionMethod
+        {
+            Awake = 0,
+            OnEnable = 1,
+            Script = 2
+        }
+        
+        [SerializeField] private InjectionMethod injectOn = InjectionMethod.Awake;
+        
         private bool _isInjected;
         
         void ISInjectorRegisterHandler.OnRegister(ISInjector injector) => _isInjected = true;
         
         private void Awake()
+        {
+            if (injectOn != InjectionMethod.Awake) return;
+            
+            InjectOnce();
+        }
+        
+        private void OnEnable()
+        {
+            if (injectOn != InjectionMethod.OnEnable) return;
+            
+            InjectOnce();
+        }
+        
+        public void Inject()
+        {
+            if (injectOn != InjectionMethod.Script) return;
+            
+            InjectOnce();
+        }
+        
+        private void InjectOnce()
         {
             if (_isInjected)
             {
@@ -26,9 +56,13 @@ namespace Sapo.DI.Runtime.Behaviours
                 return;
             }
             
+            InjectAndDestroy();
+        }
+        
+        private void InjectAndDestroy()
+        {
             var injector = SRootInjector.FindOrCreateSingleton();;
             injector.InjectScene(gameObject.scene);
-            
             Destroy();
         }
 
